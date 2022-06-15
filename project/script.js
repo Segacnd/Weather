5f5d076bf5b594dcc8169122036722479fd78620
Segacnd <segabelka@yandex.ru>
2022-06-15 16:38:28 +0400

    Create img designe

--- project/script.js
+++ project/script.js
@@ -38,10 +38,10 @@ document.querySelector('.search button').addEventListener('click', function () {
     weather.search();
 });
 
-// document.querySelector('search-bar').addEventListener('keyup', function (e) {
-//     if (e.key == 'Enter') {
-//         weather.search()
-//     }
-// })
+document.querySelector('search-bar').addEventListener('keyup', function (e) {
+    if (e.key == 'Enter') {
+        weather.search()
+    }
+})
 
 weather.fetchWether('Minsk');
